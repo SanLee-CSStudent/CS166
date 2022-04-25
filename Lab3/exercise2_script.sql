06496df098582aff2c046b79af5be32b507a64b5
San Lee <allen000525@gmail.com>
2022-04-25 15:51:53 -0700

    Update exercise2_script.sql

--- Lab3/exercise2_script.sql
+++ Lab3/exercise2_script.sql
@@ -6,14 +6,14 @@ DROP TABLE IF EXISTS Songs;
 
 --entities
 CREATE TABLE Musicians(ssn NUMERIC(10, 0), name TEXT, PRIMARY KEY(ssn));
-CREATE TABLE Album(album_id INT, title TEXT, copyright_date DATE, speed INT, PRIMARY KEY(identifier));
+CREATE TABLE Album(album_id INT, title TEXT, copyright_date DATE, speed INT, PRIMARY KEY(album_id));
 CREATE TABLE Instrument(instrument_id INT, dname TEXT, key TEXT, PRIMARY KEY(instrument_id));
 CREATE TABLE Songs(song_id INT, title TEXT, author TEXT, PRIMARY KEY(song_id));
 CREATE TABLE Place(address TEXT, PRIMARY KEY(address));
 
 --relationships
-CREATE TABLE Telephone Home(address TEXT, phone_no TEXT, PRIMARY KEY(phone_no), FOREIGN KEY(address) REFERENCES Place);
-CREATE TABLE Lives(ssn NUMERIC(10, 0), address TEXT, phone_no TEXT, PRIMARY KEY(ssn, address), FOREIGN KEY(address, phone_no) REFERENCES Home, FOREIGN KEY(ssn) REFERENCES Musicians);
+CREATE TABLE Telephone_Home(address TEXT, phone_no TEXT, PRIMARY KEY(phone_no), FOREIGN KEY(address) REFERENCES Place);
+CREATE TABLE Lives(ssn NUMERIC(10, 0), address TEXT, phone_no TEXT, PRIMARY KEY(ssn, address), FOREIGN KEY(phone_no) REFERENCES Telephone_Home, FOREIGN KEY(address) REFERENCES Place, FOREIGN KEY(ssn) REFERENCES Musicians);
 
 CREATE TABLE Plays(ssn NUMERIC(10, 0), instrument_id INT, PRIMARY KEY(ssn, instrument_id), FOREIGN KEY(ssn) REFERENCES Musicians, FOREIGN KEY(instrument_id) REFERENCES Instrument);
 
