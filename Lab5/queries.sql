8879fdef7d9a81db298630b6e7f7f02100a3d0eb
San Lee <allen000525@gmail.com>
2022-05-10 15:43:39 -0700

    Added Lab5

---
+++ Lab5/queries.sql
@@ -0,0 +1,24 @@
+--First query
+SELECT p.pid
+FROM parts p, catalog c
+WHERE p.pid = c.pid AND c.cost < 10;
+
+--Second query
+SELECT p.pname
+FROM parts p, catalog c
+WHERE p.pid = c.pid AND c.cost < 10;
+
+--Third query
+SELECT s.address
+FROM suppliers s, parts p, catalog c
+WHERE s.sid = c.sid AND c.pid = p.pid AND p.pname = "Fire Hydrant Cap";
+
+--Fourth query
+SELECT s.sname
+FROM suppliers s, parts p, catalog c
+WHERE s.sid = c.sid AND c.pid = p.pid AND p.color = "Green";
+
+--Fifth query
+SELECT s.sname, p.pname
+FROM suppliers s, parts p, catalog c
+WHERE s.sid = c.sid AND c.pid = p.pid;
