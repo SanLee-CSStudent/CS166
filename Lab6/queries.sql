58ab3edbb8506d7eae496abf09f9df83879b7051
San Lee <allen000525@gmail.com>
2022-05-16 16:59:34 -0700

    Update queries.sql

--- Lab6/queries.sql
+++ Lab6/queries.sql
@@ -19,9 +19,8 @@ GROUP BY Suppliers
 INTERSECT
 SELECT s.sname,  COUNT(*) AS PartsNum
 FROM parts p, catalog c, suppliers s
-WHERE p.pid = c.pid AND s.sid = c.sid AND s.sname NOT IN(
+WHERE s.sname NOT IN(
     SELECT s.sname
-    FROM parts p, catalog c, suppliers s
     WHERE p.pid = c.pid AND s.sid = c.sid NOT AND p.color = 'Green'
 )
-GROUP BY s.sname
+GROUP BY s.sname;
