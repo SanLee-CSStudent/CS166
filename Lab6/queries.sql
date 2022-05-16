7580f277c019250f8cb2afdafb842c450dce7310
San Lee <allen000525@gmail.com>
2022-05-16 16:44:40 -0700

    Update queries.sql

--- Lab6/queries.sql
+++ Lab6/queries.sql
@@ -17,11 +17,11 @@ HAVING COUNT(*) > 2;
 --WHERE p.pid = c.pid AND s.sid = c.sid
 --GROUP BY Suppliers
 --INTERSECT
-SELECT s.sname,  COUNT(*) AS PartsNum
-FROM parts p, catalog c, suppliers s
-WHERE p.pid = c.pid AND s.sid = c.sid
-GROUP BY s.sname
-EXCEPT
+--SELECT s.sname,  COUNT(*) AS PartsNum
+--FROM parts p, catalog c, suppliers s
+--WHERE p.pid = c.pid AND s.sid = c.sid
+--GROUP BY s.sname
+--EXCEPT
 SELECT s.sname,  COUNT(*) AS PartsNum
 FROM parts p, catalog c, suppliers s
 WHERE p.pid = c.pid AND s.sid = c.sid AND NOT p.color = 'green'
