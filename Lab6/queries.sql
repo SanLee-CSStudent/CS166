054abb2a98c74f746ca2beb279f93d52af20e2a4
San Lee <allen000525@gmail.com>
2022-05-16 16:32:10 -0700

    Update queries.sql

--- Lab6/queries.sql
+++ Lab6/queries.sql
@@ -2,20 +2,20 @@
 SELECT s.sname, COUNT(*) AS PartsNum
 FROM parts p, catalog c, suppliers s
 WHERE p.pid = c.pid AND s.sid = c.sid
-GROUP BY s.sname, PartsNum;
+GROUP BY s.sname;
 
 --Second query
 SELECT s.sname, COUNT(*) AS PartsNum
 FROM parts p, catalog c, suppliers s
 WHERE p.pid = c.pid AND s.sid = c.sid
-GROUP BY s.sname, PartsNum
+GROUP BY s.sname
 HAVING COUNT(*) > 3;
 
 --Third query
 SELECT s.sname, COUNT(*) AS PartsNum
 FROM parts p, catalog c, suppliers s
 WHERE p.pid = c.pid AND s.sid = c.sid
-GROUP BY s.sname, PartsNum
+GROUP BY s.sname
 INTERSECT
 SELECT s.sname,  COUNT(*) AS PartsNum
 FROM parts p, catalog c, suppliers s
