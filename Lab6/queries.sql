010158e9d223f6f8abdd43792b27c57c24ed923e
San Lee <allen000525@gmail.com>
2022-05-16 16:29:20 -0700

    Update queries.sql

--- Lab6/queries.sql
+++ Lab6/queries.sql
@@ -1,27 +1,27 @@
 --First query
 SELECT s.sname, COUNT(*) AS PartsNum
 FROM parts p, catalog c, suppliers s
-GROUP BY s.sname, PartsNum
 WHERE p.pid = c.pid AND s.sid = c.sid;
+GROUP BY s.sname, PartsNum
 
 --Second query
 SELECT s.sname, COUNT(*) AS PartsNum
 FROM parts p, catalog c, suppliers s
-GROUP BY s.sname, PartsNum
 WHERE p.pid = c.pid AND s.sid = c.sid
+GROUP BY s.sname, PartsNum
 HAVING COUNT(*) > 3;
 
 --Third query
 SELECT s.sname, COUNT(*) AS PartsNum
 FROM parts p, catalog c, suppliers s
-GROUP BY s.sname, PartsNum
 WHERE p.pid = c.pid AND s.sid = c.sid
+GROUP BY s.sname, PartsNum
 INTERSECT
 SELECT s.sname,  COUNT(*) AS PartsNum
 FROM parts p, catalog c, suppliers s
-GROUP BY s.sname, PartsNum
 WHERE p.pid = c.pid AND s.sid = c.sid AND s.sname NOT IN(
     SELECT s.sname
     FROM parts p, catalog c, suppliers s
     WHERE p.pid = c.pid AND s.sid = c.sid AND NOT p.color = 'green'
-);
+)
+GROUP BY s.sname, PartsNum;
