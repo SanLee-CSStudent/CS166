124d8c9e5b5f541f93aa23da7fbc249f2123c53d
San Lee <allen000525@gmail.com>
2022-05-16 18:04:15 -0700

    Update queries.sql

--- Lab6/queries.sql
+++ Lab6/queries.sql
@@ -17,10 +17,9 @@ HAVING COUNT(*) > 2;
 --WHERE p.pid = c.pid AND s.sid = c.sid
 --GROUP BY Suppliers
 --INTERSECT
-SELECT s.sname,  COUNT(*) AS PartsNum
+SELECT s.sname
 FROM parts p, catalog c, suppliers s
 WHERE s.sname NOT IN(
     SELECT s.sname
     WHERE p.pid = c.pid AND s.sid = c.sid AND NOT p.color = 'Green'
-)
-GROUP BY s.sname;
+);
