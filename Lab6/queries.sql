fde58606ec0809926a9724829992cdb0859a8610
San Lee <allen000525@gmail.com>
2022-05-16 18:24:36 -0700

    Update queries.sql

--- Lab6/queries.sql
+++ Lab6/queries.sql
@@ -29,7 +29,7 @@ GROUP BY Suppliers;
 --Fourth query
 SELECT s.sname AS Suppliers, MAX(c.cost) AS ExpensivePart
 FROM catalog c, suppliers s
-WHERE s.sname IN (
+WHERE s.sid = c.sid AND s.sname IN (
     SELECT s.sname AS Suppliers
     FROM parts p, catalog c, suppliers s
     WHERE p.pid = c.pid AND s.sid = c.sid AND p.color = 'Green'
