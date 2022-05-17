2ed7ed7a652fb38387517c03c70db4fbe8957ff6
San Lee <allen000525@gmail.com>
2022-05-16 18:10:57 -0700

    Update queries.sql

--- Lab6/queries.sql
+++ Lab6/queries.sql
@@ -12,17 +12,18 @@ GROUP BY Suppliers
 HAVING COUNT(*) > 2;
 
 --Third query
---SELECT s.sname AS Suppliers, COUNT(*) AS PartsNum
---FROM parts p, catalog c, suppliers s
---WHERE p.pid = c.pid AND s.sid = c.sid
---GROUP BY Suppliers
---INTERSECT
-SELECT s.sname AS Suppliers
-FROM suppliers s
-WHERE s.sname NOT IN (
-    SELECT s.sname
-    FROM parts p, catalog c, suppliers s
-    WHERE p.pid = c.pid AND s.sid = c.sid AND NOT p.color = 'Green'
-    GROUP BY s.sname)
+SELECT s.sname AS Suppliers, COUNT(p.pid) AS PartsNum
+FROM parts p, catalog c, suppliers s
+WHERE p.pid = c.pid AND s.sid = c.sid AND s.sname IN(
+    SELECT s.sname AS Suppliers
+    FROM suppliers s
+    WHERE s.sname NOT IN (
+        SELECT s.sname
+        FROM parts p, catalog c, suppliers s
+        WHERE p.pid = c.pid AND s.sid = c.sid AND NOT p.color = 'Green'
+        GROUP BY s.sname)
+    GROUP BY Suppliers;
+)
 GROUP BY Suppliers;
+
 
