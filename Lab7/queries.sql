dc0d6efedfff3f33d61bdc2c60e0651b66d4a837
San Lee <allen000525@gmail.com>
2022-05-22 16:51:21 -0700

    Added fifth and sixth query

--- Lab7/queries.sql
+++ Lab7/queries.sql
@@ -11,7 +11,33 @@ WHERE n.on_hand > 70
 GROUP BY Part_ID;
 
 --Second Query
-SELECT c.color_name AS Color, SUM(n.on_hand) AS total_parts
+SELECT c.color_name AS NYC_Color, SUM(n.on_hand) AS total_parts
 FROM color c, part_nyc n
 WHERE c.color_name = 'Red' AND c.color_id = n.color
-GROUP BY Color;
+GROUP BY NYC_Color
+UNION
+SELECT c.color_name AS SFO_Color, SUM(s.on_hand) AS total_parts
+FROM color c, part_sfo s
+WHERE c.color_name = 'Red' AND c.color_id = s.color
+GROUP BY SFO_Color;
+
+--Third Query
+SELECT supp.supplier_name, 
+FROM supplier supp, part_nyc n
+WHERE supp.supplier_id = n.supplier
+HAVING SUM(n.on_hand) > (SELECT SUM(s.on_hand)
+    FROM supplier supp, part_sfo s
+    WHERE supp.supplier_id = s.supplier
+);
+
+--Fourth Query
+
+
+--Fifth Query???
+UPDATE part_nyc
+SET on_hand - 10;
+
+--Sixth Query???
+DELETE
+FROM part_nyc
+WHERE on_hand < 30;
