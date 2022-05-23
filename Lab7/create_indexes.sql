5dfccc862739d375f46467ffe40a21419b9edd11
San Lee <allen000525@gmail.com>
2022-05-22 22:30:42 -0700

    Update create_indexes.sql

--- Lab7/create_indexes.sql
+++ Lab7/create_indexes.sql
@@ -1,6 +1,6 @@
-DROP INDEX index_supplier;
-DROP INDEX index_nyc;
-DROP INDEX index_sfo;
+DROP INDEX IF EXISTS index_supplier;
+DROP INDEX IF EXISTS index_nyc;
+DROP INDEX IF EXISTS index_sfo;
 
 CREATE INDEX index_supplier ON supplier [USING BTREE](supplier_name, supplier_id);
 CREATE INDEX index_nyc ON part_nyc [USING BTREE](part_number, supplier, on_hand);
